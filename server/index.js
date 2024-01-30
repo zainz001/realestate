5d81a41d5d94c87e69aba186492559268310e38d
zainz001 <uhanif30@gmail.com>
2024-01-30 16:06:06 +0500

    new

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 import express from "express";
-const app=express();
-app.listen(3000,()=>{
-console.log('server is running on 3000');
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.mongose).then(() => {
+    console.log('connected to mongodb');
+}).catch((err) => {
+    console.log(err);
+})
+const app = express();
+app.listen(3000, () => {
+    console.log('server is running on 3000');
 });
